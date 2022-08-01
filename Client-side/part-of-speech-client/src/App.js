6c40b9fa48afc511230ed11eb969aef60730dcfa
Ahmed Khames <ahmedmamdouhkhames@gmail.com>
2022-08-01 05:20:27 +0200

    improve the ui

--- Client-side/part-of-speech-client/src/App.js
+++ Client-side/part-of-speech-client/src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const ProgressBar = ({ currentQuestionIndex, totalQuestionsCount }) => {
-  const progressPercentage = (currentQuestionIndex / totalQuestionsCount) * 100;
-
-  return (
-    <div className="progressBar">
-      <div className="text">
-        {currentQuestionIndex} answered (
-        {totalQuestionsCount - currentQuestionIndex} remaining)
-      </div>
-      <div className="inner" style={{ width: `${progressPercentage}%` }} />
-    </div>
-  );
-};
 
 function App() {
   const [currentWord, setCurrentWord] = useState(0);
@@ -29,6 +19,8 @@ function App() {
   const [questions, setQuestions] = useState([]);
 
   const [again, setAgain] = useState(false);
+
+  library.add(faCheck, faTimes);
 
   useEffect(() => {
     if (finished) {
@@ -86,7 +78,6 @@ function App() {
   function selectedAnswer(e) {
     setHaveAnswered(true);
     setSelectedIdx(pos.indexOf(e.currentTarget.getAttribute("value")));
-
     if (e.currentTarget.getAttribute("value") === questions[currentWord].pos) {
       setIsCorrect(true);
       setScore(score + 1);
@@ -94,63 +85,106 @@ function App() {
       setIsCorrect(false);
     }
   }
+  const progressPercentage = (currentWord / questions.length) * 100;
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div className="spinner"></div>;
   } else {
     return (
       <div className="App">
-
-        <div className="">
-          <h1>Point of Speech</h1>
-          <p>Categorize the words according to their part of speech</p>
-        </div>
-
         {finished ? (
-
-          <div className="card">
-             <div className="container">
-            <h1>The student rank is {rank}</h1>
-            <button onClick={() => tryAgain()}>Try again</button>
+          <div className="result_box">        
+            <div className="complete_text">You've completed the Quiz!</div>
+            <div className="score_text">
+              <span>Your rank is <p>{rank}</p> </span>
+            </div>
+            <div className="buttons">
+              <button className="restart" onClick={() => tryAgain()}>
+                Try Again
+              </button>
+            </div>
           </div>
-          </div>
-
         ) : (
-          <div>
-
-            <div className="card">
-              <div className="container">
-                <h3>The word {questions[currentWord].word} is </h3>
-
-                <div className="answers">
-                  {pos.map((answer, index) => {
-                    return (
-                      <div
-                        key={index}
-                        value={answer}
-                        onClick={(e) =>
-                          haveAnswered ? e.preventDefault : selectedAnswer(e)
-                        }
-                        className={`answer ${
-                          haveAnswered
-                            ? index === selectedIdx
-                              ? isCorrect
-                                ? `correct noHover`
-                                : `incorrect noHover`
-                              : `answer noHover`
-                            : `answer`
-                        }`}
-                      >
-                        {answer}
-                      </div>
-                    );
-                  })}
+          <div className="quiz_box activeQuiz">
+            <header>
+              <div className="title">Point of Speech</div>
+              <div class="timer">
+                <div class="time_left_txt">Correct answers</div>
+                <div class="timer_sec">
+                  {score}
                 </div>
               </div>
-            </div>
-                  
+              <div
+                className="time_line"
+                style={{ width: `${progressPercentage}%` }}
+              ></div>
+            </header>
+            <section>
+              <div className="que_text">
+                <span>
+                  {currentWord + 1}. The word {questions[currentWord].word} is
+                </span>
+              </div>
+              <div className="option_list">
+                {pos.map((answer, index) => {
+                  return (
+                    <div
+                      key={index}
+                      value={answer}
+                      onClick={(e) =>
+                        haveAnswered ? e.preventDefault : selectedAnswer(e)
+                      }
+                      className={`option ${
+                        haveAnswered
+                          ? index === selectedIdx
+                            ? isCorrect
+                              ? `correct disabled`
+                              : `incorrect disabled`
+                            : `disabled`
+                          : ``
+                      }`}
+                    >
+                      {haveAnswered ? (
+                        index === selectedIdx ? (
+                          isCorrect ? (
+                            <div>
+                              <FontAwesomeIcon icon="fa-solid fa-check" />
+                              {" " + answer}
+                            </div>
+                          ) : (
+                            <div>
+                              <FontAwesomeIcon icon="fa-solid fa-times" />
+                              {" " + answer}
+                            </div>
+                          )
+                        ) : (
+                          answer
+                        )
+                      ) : (
+                        answer
+                      )}
+                      
+                    </div>
+                  );
+                })}
+              </div>
+            </section>
+            <footer>
+              <div className="total_que">
+                <span>
+                  <p> {currentWord + 1} </p> of <p> {questions.length}</p>{" "}
+                  Questions
+                </span>
+              </div>
+
+             
+              <button onClick={() => next()} className={currentWord  === questions.length -1?`submit`:''}>
+              {currentWord + 1 < questions.length?`Next Word`:'Submit'}
+              </button>
+            </footer>
+
             {/* {haveAnswered ? (
               isCorrect ? (
                 <div>correct</div>
@@ -160,13 +194,13 @@ function App() {
             ) : (
               <div></div>
             )} */}
-          
+            {/* 
             <button onClick={() => next()}>Next word</button>
             <br />
             <ProgressBar
               currentQuestionIndex={currentWord}
               totalQuestionsCount={questions.length}
-            ></ProgressBar>
+            ></ProgressBar> */}
           </div>
         )}
       </div>
